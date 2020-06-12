cbc8b42751d7c5e28eafe8c1d39b482a085af48f
Michael Guenther <58656683+michaguenther@users.noreply.github.com>
2020-06-12 19:40:13 +0200

    dockerscript

---
+++ scripts/docker-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$1" == "build" ] ; then
+    docker build -t ikom/helfertool .
+elif [ "$1" == "create" ] ; then
+    docker run -d \
+    --name ikomhelfertool \
+    --restart always \
+    -v "$PWD/mounts/data:/data" \
+    -v "$PWD/mounts/config:/config" \
+    -v "$PWD/mounts/log:/log" \
+    -p 127.0.0.1:9000:8000 ikom/helfertool
+elif [ "$1" == "start" ] ; then
+    docker start ikomhelfertool
+elif [ "$1" == "stop" ] ; then
+    docker stop ikomhelfertool
+fi
+
